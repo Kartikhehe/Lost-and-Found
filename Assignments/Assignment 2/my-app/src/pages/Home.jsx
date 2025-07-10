22063552c004fc8b715018a5f80d30a7d6dce348
FarhanAktherR <110246567+FarhanAktherR@users.noreply.github.com>
2025-07-10 17:26:20 +0530

    Assignment 2 submission

---
+++ Assignments/Assignment 2/my-app/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Cards from '../components/Cards.jsx'
+import Footer from '../components/Footer.jsx'
+
+function Home() {
+  return (
+    <>
+      <Cards />
+      <Footer />
+    </>
+  )
+}
+
+export default Home;
