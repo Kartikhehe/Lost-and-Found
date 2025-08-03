0c47b6df135a4d69a1ad03e579d22af25d96122d
FarhanAktherR <110246567+FarhanAktherR@users.noreply.github.com>
2025-08-03 14:33:39 +0530

    Update server.js

--- Assignments/Assignment 3/Assignment_3_240389/server/server.js
+++ Assignments/Assignment 3/Assignment_3_240389/server/server.js
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:5173', 'https://your-frontend.vercel.app'], credentials: true }));
 app.use(express.json());
 
 const pool = new Pool({
